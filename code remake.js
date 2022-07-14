0289c4d87501ec8b832bdbc6d5f6c6eb05bc9085
CamFaison <102275372+CamFaison@users.noreply.github.com>
2022-07-14 13:50:05 -0700

    Add files via upload

---
+++ code remake.js
@@ -0,0 +1,79 @@
+//function for countdownTimer V1 for wk1-2
+function start(){
+    console.log("start() function started");
+    // created variable currTime set it equal to 10
+    var currTime = 10;
+    console.log(currTime);
+    document.getElementById("countdownStatus").innerHTML = currTime;
+    setTimeout(function(){
+        currTime = currTime - 1;
+        //what we do after the delay
+        document.getElementById("countdownStatus").innerHTML = currTime;
+        console.log(currTime);
+    }, 1000)
+    
+    setTimeout(function(){
+        currTime = currTime - 1;
+        //what we do after the delay
+        document.getElementById("countdownStatus").innerHTML = currTime;
+        console.log(currTime);
+    }, 2000)
+    
+    setTimeout(function(){
+        currTime = currTime - 1;
+        //what we do after the delay
+        document.getElementById("countdownStatus").innerHTML = currTime;
+        console.log(currTime);
+    }, 3000)
+    
+    setTimeout(function(){
+        currTime = currTime - 1;
+        //what we do after the delay
+        document.getElementById("countdownStatus").innerHTML = currTime;
+        console.log(currTime);
+    }, 4000)
+    
+    setTimeout(function(){
+        currTime = currTime - 1;
+        //what we do after the delay
+        document.getElementById("countdownStatus").innerHTML = currTime;
+        console.log(currTime);
+    }, 5000)
+    
+    setTimeout(function(){
+        currTime = currTime - 1;
+        //what we do after the delay
+        document.getElementById("countdownStatus").innerHTML = currTime;
+        console.log(currTime);
+    }, 6000)
+    
+    setTimeout(function(){
+        currTime = currTime - 1;
+        //what we do after the delay
+        document.getElementById("countdownStatus").innerHTML = currTime;
+        console.log(currTime);
+    }, 7000)
+
+    setTimeout(function(){
+        currTime = currTime - 1;
+        //what we do after the delay
+        document.getElementById("countdownStatus").innerHTML = currTime;
+        console.log(currTime);
+    }, 8000)
+
+    setTimeout(function(){
+        currTime = currTime - 1;
+        //what we do after the delay
+        document.getElementById("countdownStatus").innerHTML = currTime;
+        console.log(currTime);
+    }, 9000)
+
+    setTimeout(function(){
+        currTime = currTime - 1;
+        //what we do after the delay
+        document.getElementById("countdownStatus").innerHTML = currTime;
+        console.log(currTime);
+    }, 10000)
+
+      }  
+   
